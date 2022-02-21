18710e8a68883f2848c6a14fdbd687614783155a
zac <91054519+maneszjt@users.noreply.github.com>
2022-02-22 00:08:13 +1000

    Update wordDrawer.py

--- wordDrawer.py
+++ wordDrawer.py
@@ -1,34 +1,35 @@
+# importing the PIL library
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-wordsInScript = []
-fileName = './test.txt'
+wordsInScript = list()
+fileName = "test.txt"
+
+openFile = open(fileName)
+for word in openFile.read().split():    
+    wordsInScript.append(word)
+openFile.close()
 
 def drawImage(word):
+    myFont = ImageFont.truetype('/System/Library/Fonts/Supplemental/Courier New.ttf', 175)
+    
     # open an image
-    img = Image.open('./test.png')
+    img = Image.open('base.jpg')
 
     # call Draw method to add 2D graphics to an image
     I1 = ImageDraw.Draw(img)
 
-    myFont = ImageFont.load_default(125)
-
     # add text to image
-    I1.text((179, 500), word, fill=(242,8,8))
+    I1.text((540, 540), word, fill="red")
 
     # display edited image
     img.show()
 
     # save the edited image
-    img.save[f"{word}.png"]
+    img.save(f"./output/{word}.jpg")
 
-def countWordsInFile():
-    openFile = open(fileName, "r")
-    i = openFile
-    for words in i.read().split():
-        for word in words:
-            wordsInScript.append[word]
+for words in wordsInScript:
+    drawImage(words)
 
-for words in wordsInScript([]):
-    drawImage[words]
+print("The list of words is: ", wordsInScript)
